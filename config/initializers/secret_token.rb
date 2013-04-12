d785b491f1a7d2c1d52c5f2024d03c993be24c85
Invalid <carpk@gmx.com>
2013-04-11 20:04:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e82659239bb8e99305da5c37b2216f07ca0ae854231d61c9f31d15b82af7954cc5a7c7a6374be7a228824177a6f990bdd63fd9da7bb9e7c5f295d3b414e0eff0'
